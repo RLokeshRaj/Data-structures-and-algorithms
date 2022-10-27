a40a02f6802b531115d5aec9396ab166e9095246
RLokeshRaj <115402407+RLokeshRaj@users.noreply.github.com>
2022-10-27 07:15:17 +0530

    Update Assigncookies.java

--- Assigncookies.java
+++ Assigncookies.java
@@ -4,6 +4,12 @@ import java.util.Arrays;
 
 public class Assigncookies {
 	
+	
+	/*
+	https://leetcode.com/problems/assign-cookies/
+	/*
+	
+	 --- Pseudo Code ---
 	//Initialize the arrays
 	//Sort the arrays
 	// Use two pointers ( int p1 and p2)
@@ -12,10 +18,26 @@ public class Assigncookies {
 	// else condition to check if p1<p2
 	
 
-public static void main(String[] args) {
+@Test
+	public void example1() {
+		//Positive Test Data
+		int[] arr1 = {1,2,3};
+		int[] arr2 = {1,1};
+		int output = 1;
+		Assert.assertEquals(assignCookies(g,s), output);
+	}
+	
+	@Test
+	public void example2() {
 		
-		int[] arr1 = {1,1,2,3};
-		int[] arr2 = {1,2};
+		int[] arr1 = {1,2};
+		int[] arr12 = {1,2,3};
+		int output = 1;
+		Assert.assertEquals(assignCookies(arr1,arr2), output);
+	}
+	
+private Object assignCookies(int[] arr1, int[] arr2) {
+		
 		Arrays.sort(arr1);
 		Arrays.sort(arr2);
 		int p1= 0;
